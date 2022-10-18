a2b8aa8821445e92684355f930ca9043a0486a50
Codrescu, Marcus (mc678p) <mc678p@att.com>
2022-10-18 12:03:45 -0500

    Added temporary table default option when uploading CSV files.

--- app.R
+++ app.R
@@ -876,6 +876,12 @@ server <- function(input, output, session) {
             "Clean column names?",
             choices = c("Yes", "No"),
             selected = "Yes"
+          ),
+          selectInput(
+            "tempTable",
+            "Temporary table?",
+            choices = c("Yes", "No"),
+            selected = "Yes"
           )
         ),
         footer = tagList(
@@ -900,7 +906,8 @@ server <- function(input, output, session) {
           dbWriteTable(con,
             name = Id(table = input$newTableName, schema = input$schema),
             value = data.frame(new_table),
-            overwrite = TRUE
+            overwrite = TRUE,
+            temporary = ifelse(input$tempTable == "Yes", TRUE, FALSE)
           )
           result <- "Success"
         },
